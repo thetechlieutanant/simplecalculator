88bb92e570deb172b7a44cb4e6c56b1d76488f43
thetechlieutanant <140601627+thetechlieutanant@users.noreply.github.com>
2023-08-16 21:32:18 +0530

    Add files via upload

---
+++ Simple calculator/script.js
@@ -0,0 +1,30 @@
+let displayValue = "0";
+
+function updateDisplay() {
+  document.getElementById("display").innerText = displayValue;
+}
+
+function appendToDisplay(value) {
+  if (displayValue === "0") {
+    displayValue = value;
+  } else {
+    displayValue += value;
+  }
+  updateDisplay();
+}
+
+function clearDisplay() {
+  displayValue = "0";
+  updateDisplay();
+}
+
+function calculate() {
+  try {
+    const result = eval(displayValue);
+    displayValue = result.toString();
+    updateDisplay();
+  } catch (error) {
+    displayValue = "Error";
+    updateDisplay();
+  }
+}
